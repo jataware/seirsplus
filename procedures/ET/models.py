4fba2b2950a1584cf877f05efbdba7ac0b22a699
Travis Hartman <63281380+travis-jat@users.noreply.github.com>
2021-02-10 15:01:42 -0800

    del import

--- procedures/ET/models.py
+++ procedures/ET/models.py
@@ -2,7 +2,6 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import networkx as networkx
 import numpy as numpy
 import scipy as scipy
 import scipy.integrate
